750f89f6b9a891fcb1f0b8d9f25d1a7b5fd4ebac
Tim Kharshan <timkharshan@hotmail.com>
2017-10-22 20:10:45 -0700

    W4D1 hw

---
+++ W4D1/app/controllers/books_controller.rb
@@ -0,0 +1,33 @@
+class BooksController < ApplicationController
+  def index
+    @books=Book.all
+    # render :index
+  end
+
+  def new
+    render :new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_url
+  end
+
+  def destroy
+    begin
+      book = Book.find(params[:id])
+      book.destroy
+    rescue
+      puts "couldnt find that book"
+    end
+    redirect_to books_url
+
+
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :author)
+  end
+end

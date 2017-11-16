272423b0169b3a40187db924977041a6bd5e0d26
Tim Kharshan <timkharshan@hotmail.com>
2017-11-15 21:24:52 -0800

    rainbow routes

---
+++ w7d4/skeleton/components/rainbow.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {
+  Route,
+  Link,
+  NavLink
+} from 'react-router-dom';
+
+import Red from './red';
+import Green from './green';
+import Blue from './blue';
+import Violet from './violet';
+
+class Rainbow extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Rainbow Router!</h1>
+        <Link to='/red' >Red</Link>
+        <Link to='/green' >Green</Link>
+        <Link to='/blue' >Blue</Link>
+        <Link to='/violet' >Violet</Link>
+
+
+        <div id="rainbow">
+        <Route path="/red" component={Red} />
+        <Route path="/Green" component={Green} />
+        <Route path="/Blue" component={Blue} />
+        <Route path="/Violet" component={Violet} />
+        </div>
+      </div>
+    );
+  }
+};
+
+export default Rainbow;
